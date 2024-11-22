e9863f1ae54ac37240921ff5628dfa17c484fbf2
Julien Meyer <jadeware@gmail.com>
2024-11-22 01:02:00 +0100

    no message

--- examples/Pong-iPhone/Classes/ball.h
+++ examples/Pong-iPhone/Classes/ball.h
@@ -14,6 +14,23 @@
 #import "vector.h"
 #import "KGraphic.h"
 
+#ifdef LF_ANDROID_BUILD
+#include "Box2D/Box2D.h"
+#include <android/asset_manager.h>
+#include <android/asset_manager_jni.h>
+#else
+#include <Box2D/Box2D.h>
+#endif
+
+#define PTM_RATIO        32.0 // box2d pixel to meter
+#define EDGE_IS_GROUND    (char*)"10002"
+
+#define CATEGORY_WALL        0x0001
+#define CATEGORY_GROUND        0x0002
+#define CATEGORY_BLOCK        0x0004
+#define CATEGORY_BLOCKER    0x0008
+
+
 class ball : Node
 {
 private:
@@ -21,9 +38,18 @@ private:
     int screenWidth;
     int screenHeight;
     
+    b2Body * box2d_body;
+    b2BodyDef ballBodyDef;
+    b2Fixture * fixture;
+    b2World * _world;
+    b2ContactListener * contactListener;
+    float density=1.0f;
+    float * vertices;
+    int numberOfVertices;
+    
 public:
     
-    Vector * pos;
+    //Vector * pos;
     Vector * vel;
     Vector * siz;
 	
@@ -33,6 +59,8 @@ public:
     ball(int posx, int posy, float vx, float vy, int w, int h, int game_w, int game_h, int screen_w, int screen_h);
     ~ball();
     
+    Vector getPos();
+    void editFixture(float density, float friction, float restitution);
     void update(float frametime);
 };
 
